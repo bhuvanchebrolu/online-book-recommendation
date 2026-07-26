ec3d10ef22257faacb514d03d7ef34806b7790db
bhuvan <bhuvanchebrolu@gmail.com>
2026-07-26 14:05:14 +0530

    small changes cors

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,10 @@ app.use(cookieParser());
 // 2. CORS — must be before routes
 app.use(
   cors({
-    origin:         "http://localhost:5173",
+     origin: [
+    "http://localhost:5173",
+    "https://online-book-recommendation.vercel.app",
+  ],
     credentials:    true,
     methods:        ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
